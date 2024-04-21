d53fab09e1b6a1e5d0082f88c57b272d7a086f64
sentongo-web <134306188+sentongo-web@users.noreply.github.com>
2024-04-21 17:30:30 +0300

    ok

--- python simple refreshers/roller_coaster_app.py
+++ python simple refreshers/roller_coaster_app.py
@@ -1,7 +1,12 @@
 print ("welcome to the roller coaster app")
 height = int (input("please input your height in metres"))
 
-if height > 120:
+if height >= 120:
     print ("You can ride the roller coaster")
+    age = int(input("please input your age"))
+    if age <12:
+        print ("please pay 5$!")
+    elif age <=18:
+        print("please pay 10 $")
 else:
     print("sorry, you need to grow taller to ride the roller coaster")
