ac5afa3b7d7e0f21f9fb2f32888f749cab1415e3
sentongo-web <134306188+sentongo-web@users.noreply.github.com>
2024-04-21 16:44:54 +0300

    new

---
+++ python simple refreshers/roller_coaster_app.py
@@ -0,0 +1,7 @@
+print ("welcome to the roller coaster app")
+height = int (input("please input your height in metres"))
+
+if height > 120:
+    print ("You can ride the roller coaster")
+else:
+    print("sorry, you need to grow taller to ride the roller coaster")
---
+++ python simple refreshers/simple app.py
@@ -0,0 +1,18 @@
+print ("WELCOME TO MY QUIZZ")
+city = input("Where do you come from? \n")
+pet = input("What is your favorite pet? \n")
+print ("Your band name could be " + city + " " + pet)
+
+##### BMI CALCULATOR
+
+# height = input()
+# weight = input()
+
+# height = float (height)
+# weight = int (weight)
+
+# bmi = (weight)/(height ** 2)
+
+# bmi_rounded = round (bmi)
+
+# print (bmi_rounded)
